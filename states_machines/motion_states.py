a97b5849394a68916749654e85da730cb715eae1
DataMinion4 <cesar.ruiz6@upr.edu>
2023-02-05 18:34:06 -0400

    more changes on archuitecture.

---
+++ states_machines/motion_states.py
@@ -0,0 +1,15 @@
+#TODO: Put the motion States Machines in this file
+# Examples of such states are:
+# - Spin State
+# - Wait State or Stop State
+# - MoveDown State
+# - MoveUp State
+# - MoveLeft State
+# - MoveRight State
+# - MoveForward State
+# - MoveBackward State
+# - MoveToTarget State
+
+# These States Machines need to implement the MoveIt Planning Interface 
+# https://ros-planning.github.io/moveit_tutorials/doc/move_group_python_interface/move_group_python_interface_tutorial.html
+#  https://github.com/DukeRobotics/robosub-ros/tree/master/onboard/catkin_ws/src
